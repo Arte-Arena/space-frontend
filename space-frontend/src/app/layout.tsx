e1c0bf389c9076bedfb759b11f7c72057984fa40
Leandro Damasio <ldamasio@gmail.com>
2024-11-06 14:09:35 -0300

    feat: Add modernize template

--- space-frontend/src/app/layout.tsx
+++ space-frontend/src/app/layout.tsx
@@ -1,52 +1,22 @@
-import React from 'react';
-import { AppRouterCacheProvider } from '@mui/material-nextjs/v15-appRouter';
-import { ThemeProvider } from '@mui/material/styles';
-import theme from '../theme';
-import type { Metadata } from "next";
-import "./globals.css";
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import { Roboto } from 'next/font/google'
-import NavBar from "@/components/Navbar";
-import { Box } from '@mui/material';
-
-const roboto = Roboto({
-  weight: ['300', '400', '500', '700'],
-  subsets: ['latin'],
-  display: 'swap',
-  variable: '--font-roboto',
-});
-
-export const metadata: Metadata = {
-  title: "Arte Arena - Space",
-  description: "Espaço de Trabalho Colaborativo",
-};
+"use client";
+import { baselightTheme } from "@/utils/theme/DefaultColors";
+import { ThemeProvider } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body className={roboto.variable}>
-        <AppRouterCacheProvider>
-          <ThemeProvider theme={theme}>
-            <Box sx={{ p: 2 }}>
-              <header>
-                <NavBar />
-              </header>
-              <main>
-                {children}
-              </main>
-              <footer>
-              </footer>
-            </Box>
-          </ThemeProvider>
-        </AppRouterCacheProvider>
+      <body>
+        <ThemeProvider theme={baselightTheme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          {children}
+        </ThemeProvider>
       </body>
-    </html >
+    </html>
   );
 }
