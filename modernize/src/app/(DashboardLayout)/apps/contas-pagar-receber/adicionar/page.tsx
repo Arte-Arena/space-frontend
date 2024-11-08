206a21793b3ca2b349d11ad8da45984b08de0c73
Leandro Damasio <ldamasio@gmail.com>
2024-11-08 17:05:39 -0300

    feat: Update logos and favicon

--- modernize/src/app/(DashboardLayout)/apps/contas-pagar-receber/adicionar/page.tsx
+++ modernize/src/app/(DashboardLayout)/apps/contas-pagar-receber/adicionar/page.tsx
@@ -1,5 +1,5 @@
 'use client'
-import React from 'react';
+import React, { useState } from 'react';
 import Breadcrumb from '@/app/(DashboardLayout)/layout/shared/breadcrumb/Breadcrumb';
 import PageContainer from '@/app/components/container/PageContainer';
 import { Button } from '@mui/material';
@@ -10,21 +10,57 @@ import ParentCard from '@/app/components/shared/ParentCard';
 
 const ContasPagarReceberAdicionarScreen = () => {
 
-  
+  const [titulo, setTitulo] = useState('');
+  const [descricao, setDescricao] = useState('');
+  const [valor, setValor] = useState('');
+  const [data_vencimento, setDataVencimento] = useState('');
+  const [status, setStatus] = useState('');
+  const [tipo, setTipo] = useState('');
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const accessToken = localStorage.getItem('accessToken');
+
+    if (!accessToken) {
+      console.error('Access token not found');
+      return;
+    }
+
+    try {
+      const response = await fetch('http://api-homolog.spacearena.net/api/conta', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify({
+          titulo,
+          descricao,
+          valor: parseFloat(valor),
+          data_vencimento,
+          status,
+          tipo,
+        }),
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to create account');
+      }
+
+      console.log('Account created successfully');
+    } catch (error) {
+      console.error('Error creating account:', error);
+    }
+  };
 
   return (
     <PageContainer title="Contas a Pagar e a Receber / Adicionar" description="Contas a Pagar e a Receber da Arte Arena">
       <Breadcrumb title="Contas a Pagar e a Receber / Adicionar" subtitle="Gerencie as contas a pagar e a receber da Arte Arena / Adicionar" />
       <ParentCard title="Adicionar Nova Conta" >
-        <form style={{ maxWidth: '600px' }}>
+        <form style={{ maxWidth: '600px' }} action="" onSubmit={handleSubmit}>
 
 
-        {/* 'titulo',
-        'descricao',
-        'valor',
-        'data_vencimento',
-        'status',
-        'tipo', */}
           <CustomFormLabel
             sx={{
               mt: 0,
@@ -38,6 +74,7 @@ const ContasPagarReceberAdicionarScreen = () => {
             helperText="Título da conta a pagar ou a receber."
             variant="outlined"
             fullWidth
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTitulo(e.target.value)}
           />
 
           <CustomFormLabel 
@@ -51,6 +88,7 @@ const ContasPagarReceberAdicionarScreen = () => {
             sx={{
               mb: '10px',
             }}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setValor(e.target.value)}
           />
 
           <CustomFormLabel
@@ -67,6 +105,7 @@ const ContasPagarReceberAdicionarScreen = () => {
             sx={{
               mb: '10px',
             }}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setDataVencimento(e.target.value)}
           />
 
           <CustomFormLabel
@@ -81,6 +120,7 @@ const ContasPagarReceberAdicionarScreen = () => {
             sx={{
               mb: '10px',
             }}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setTipo(e.target.value)}
           >
             <option value="Pendente">A Pagar</option>
             <option value="Pago">A Receber</option>
@@ -98,6 +138,7 @@ const ContasPagarReceberAdicionarScreen = () => {
             sx={{
               mb: '10px',
             }}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setStatus(e.target.value)}
           >
             <option value="Pendente">Pendente</option>
             <option value="Pago">Pago</option>
@@ -105,7 +146,12 @@ const ContasPagarReceberAdicionarScreen = () => {
           </CustomSelect>
 
           <div style={{ marginTop: '20px' }}>
-            <Button color="primary" variant="contained">
+            <Button 
+              color="primary" 
+              variant="contained"
+              type="submit"
+
+            >
               Adicionar Conta
             </Button>
           </div>
