0cb55ae4d31d04f245a4b445ab3162c461c4b1bc
Leandro Damasio <ldamasio@gmail.com>
2024-11-13 14:49:34 -0300

    feat: Try to config env var at nextConfig

--- modernize/next.config.mjs
+++ modernize/next.config.mjs
@@ -3,6 +3,9 @@ const nextConfig = {
     eslint: {
         ignoreDuringBuilds: true,
     },
+    env: {
+        NEXT_PUBLIC_API: process.env.NEXT_PUBLIC_API,
+    },
 }
 
 export default nextConfig;
