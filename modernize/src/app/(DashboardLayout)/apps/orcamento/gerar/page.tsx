09651e4861be30f095e520c60416bef4ecb72c88
Leandro Damasio <ldamasio@gmail.com>
2024-12-17 17:17:33 -0300

    feat: Remove search for products on large dataset

--- modernize/src/app/(DashboardLayout)/apps/orcamento/gerar/page.tsx
+++ modernize/src/app/(DashboardLayout)/apps/orcamento/gerar/page.tsx
@@ -65,14 +65,9 @@ const OrcamentoGerarScreen = () => {
   const isLoggedIn = useAuth();
   const [clientId, setClientId] = useState('');
   const [allClients, setAllClients] = useState<Cliente[]>([]);
-  const [produtosData, setProdutosData] = useState<Product[]>([]); // Todos os dados da API
-  const [openProduct, setOpenProduct] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [inputValue, setInputValue] = useState('');
-  const [options, setOptions] = useState<Product[]>([]);
-  const [filteredOptions, setFilteredOptions] = useState<Product[]>([]); // Declare filteredOptions
-  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null); // Armazenando o produto selecionado
   const [productsList, setProductsList] = useState<Product[]>([]);
+  const [inputValue, setInputValue] = useState('');
+  const [selectedProduct, setSelectedProduct] = useState<Product | null>(null); // Armazenando o produto selecionado
   const [openBudget, setOpenBudget] = React.useState(false);
   const [scroll, setScroll] = React.useState<DialogProps['scroll']>('paper');
   const [orçamentoTexto, setOrçamentoTexto] = useState('');
@@ -84,12 +79,10 @@ const OrcamentoGerarScreen = () => {
   const [precoSedex, setPrecoSedex] = useState('');
   const [openAlert, setOpenAlert] = useState(false);
   const descriptionElementRef = React.useRef<HTMLElement>(null);
+
+
   const accessToken = localStorage.getItem('accessToken');
-  const [query, setQuery] = useState<string>('');
-  const [searchQuery, setSearchQuery] = useState<string>(''); // Mantém a query atual usada na API
-  const [page, setPage] = useState<number>(1);
-  
-  // Consulta para clientes (sem parâmetros)
+
   const { isFetching: isFetchingClients, error: errorClients, data: dataClients } = useQuery({
     queryKey: ['clientData'],
     queryFn: () =>
@@ -102,15 +95,14 @@ const OrcamentoGerarScreen = () => {
       }).then((res) => res.json()),
   });
 
-  // Log dos dados recebidos de clientes
   useEffect(() => {
     if (dataClients) {
-      console.log('dataClients:', dataClients); // Log para ver a estrutura completa
+      console.log('dataClients:', dataClients);
       if (dataClients) {
         console.log('Dados de Clientes:', dataClients);
 
         if (Array.isArray(dataClients)) {
-          setAllClients(dataClients.map((item: Cliente) => ({ number: item.number, contact_name: item.contact_name, channel: item.channel, agent_name: item.agent_name})));
+          setAllClients(dataClients.map((item: Cliente) => ({ number: item.number, contact_name: item.contact_name, channel: item.channel, agent_name: item.agent_name })));
         } else {
           console.error('Dados inválidos recebidos da API:', dataClients);
         }
@@ -119,64 +111,111 @@ const OrcamentoGerarScreen = () => {
       }
     }
   }, [dataClients]);
-  
-  // Log do estado allClients
+
   useEffect(() => {
     if (allClients) {
       console.log('allClients:', allClients);
     }
   }, [allClients]);
 
-
-  // Consulta para produtos
-  const { isFetching: isFetchingProducts, error: errorProducts, data: dataProducts } = useQuery({
-    queryKey: ['repoData', searchQuery, page],
-    queryFn: () =>
-      fetch(`${process.env.NEXT_PUBLIC_API}/api/produto?q=${encodeURIComponent(searchQuery)}&page=${page}`, {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${accessToken}`,
-        },
-      }).then((res) => res.json()),
-  });
-
-
-  // Verificação de carregamento e erro
-  if (isFetchingProducts || isFetchingClients) return <CircularProgress />;
-  if (errorProducts) return <p>Ocorreu um erro ao buscar produtos: {errorProducts.message}</p>;
-  if (errorClients) return <p>Ocorreu um erro ao buscar clientes: {errorClients.message}</p>;
-
-  // Atualiza opções com base nos dados obtidos
-  // useEffect(() => {
-  //   if (dataProducts?.produtos) {
-  //     setOptions(dataProducts.produtos);
-  //   } else {
-  //     setOptions([]);
-  //   }
-  // }, [dataProducts]);
-
-  const handleSearch = () => {
-    setSearchQuery(query); // Update search query
-    setPage(1); // Reset to first page on new search
-  };
-
-  const handleSearchKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
-    if (event.key === 'Enter') {
-      handleSearch();
+  useEffect(() => {
+    if (clientId) {
+      console.log('clientId:', clientId);
     }
+  }, [clientId]);
+
+  useEffect(() => {
+    if (allClients) {
+      console.log('allClients:', allClients);
+    }
+  }, [allClients]);
+
+  const optionsProducts = [
+    { id: 1, nome: 'Bandeira Personalizada', preco: 10.99, prazo: 3, peso: 0.5, comprimento: 10, largura: 5, altura: 5, quantidade: 1 },
+    { id: 2, nome: 'Uniformes Personalizados', preco: 20.99, prazo: 5, peso: 1, comprimento: 20, largura: 10, altura: 10, quantidade: 1 },
+    { id: 3, nome: 'Bandeiras Politicas', preco: 30.99, prazo: 7, peso: 1.5, comprimento: 30, largura: 15, altura: 15, quantidade: 1 },
+    { id: 4, nome: 'Bandeiras Politicas para Carros', preco: 40.99, prazo: 10, peso: 2, comprimento: 40, largura: 20, altura: 20, quantidade: 1 },
+    { id: 5, nome: 'Windbanners', preco: 50.99, prazo: 15, peso: 2.5, comprimento: 50, largura: 25, altura: 25, quantidade: 1 },
+    { id: 6, nome: 'Faixas de Mão', preco: 60.99, prazo: 20, peso: 3, comprimento: 60, largura: 30, altura: 30, quantidade: 1 },
+    { id: 7, nome: 'Bandanas', preco: 70.99, prazo: 25, peso: 3.5, comprimento: 70, largura: 35, altura: 35, quantidade: 1 },
+    { id: 8, nome: 'Balaclavas', preco: 80.99, prazo: 30, peso: 4, comprimento: 80, largura: 40, altura: 40, quantidade: 1 },
+    { id: 9, nome: 'Flâmulas', preco: 90.99, prazo: 35, peso: 4.5, comprimento: 90, largura: 45, altura: 45, quantidade: 1 },
+    { id: 10, nome: 'Estandartes', preco: 100.99, prazo: 40, peso: 5, comprimento: 100, largura: 50, altura: 50, quantidade: 1 },
+    { id: 11, nome: 'Canecas de Porcelana', preco: 110.99, prazo: 45, peso: 5.5, comprimento: 110, largura: 55, altura: 55, quantidade: 1 },
+    { id: 12, nome: 'Canecas de Alumínio', preco: 120.99, prazo: 50, peso: 6, comprimento: 120, largura: 60, altura: 60, quantidade: 1 },
+    { id: 13, nome: 'Tirantes de Caneca', preco: 130.99, prazo: 55, peso: 6.5, comprimento: 130, largura: 65, altura: 65, quantidade: 1 },
+    { id: 14, nome: 'Faixa de Campeão', preco: 140.99, prazo: 60, peso: 7, comprimento: 140, largura: 70, altura: 70, quantidade: 1 },
+    { id: 15, nome: 'Chinelo Slide', preco: 150.99, prazo: 65, peso: 7.5, comprimento: 150, largura: 75, altura: 75, quantidade: 1 },
+    { id: 16, nome: 'Chinelo de Dedo', preco: 160.99, prazo: 70, peso: 8, comprimento: 160, largura: 80, altura: 80, quantidade: 1 },
+    { id: 17, nome: 'Faixa de Capitão', preco: 170.99, prazo: 75, peso: 8.5, comprimento: 170, largura: 85, altura: 85, quantidade: 1 },
+    { id: 18, nome: 'Samba Canção', preco: 180.99, prazo: 80, peso: 9, comprimento: 180, largura: 90, altura: 90, quantidade: 1 },
+    { id: 19, nome: 'Short Doll', preco: 190.99, prazo: 85, peso: 9.5, comprimento: 190, largura: 95, altura: 95, quantidade: 1 },
+    { id: 20, nome: 'Sacochila', preco: 200.99, prazo: 90, peso: 10, comprimento: 200, largura: 100, altura: 100, quantidade: 1 },
+    { id: 21, nome: 'Coletes', preco: 210.99, prazo: 95, peso: 10.5, comprimento: 210, largura: 105, altura: 105, quantidade: 1 },
+    { id: 22, nome: 'Abadás', preco: 220.99, prazo: 100, peso: 11, comprimento: 220, largura: 110, altura: 110, quantidade: 1 },
+    { id: 23, nome: 'Cachecol', preco: 230.99, prazo: 105, peso: 11.5, comprimento: 230, largura: 115, altura: 115, quantidade: 1 },
+    { id: 24, nome: 'Bandeiras de Mesa', preco: 240.99, prazo: 110, peso: 12, comprimento: 240, largura: 120, altura: 120, quantidade: 1 },
+    { id: 26, nome: 'Bandeiras para Carros', preco: 260.99, prazo: 120, peso: 13, comprimento: 260, largura: 130, altura: 130, quantidade: 1 },
+    { id: 27, nome: 'Almofadas', preco: 270.99, prazo: 125, peso: 13.5, comprimento: 270, largura: 135, altura: 135, quantidade: 1 },
+    { id: 28, nome: 'Roupão', preco: 280.99, prazo: 130, peso: 14, comprimento: 280, largura: 140, altura: 140, quantidade: 1 },
+    { id: 29, nome: 'Toalhas', preco: 290.99, prazo: 135, peso: 14.5, comprimento: 290, largura: 145, altura: 145, quantidade: 1 }
+  ];
+
+  const productNames = optionsProducts.map((product) => product.nome);
+
+  useEffect(() => {
+    if (selectedProduct) {
+      console.log('selectedProduct:', selectedProduct);
+    }
+  }, [selectedProduct]);
+
+  useEffect(() => {
+    if (productsList) {
+      console.log('productsList:', productsList);
+    }
+  }, [productsList]);
+
+  function adicionarProduto(novoProduto: Product) {
+
+    // Se já está na productsList, não adiciona novamente, soma 1 na sua quantidade.
+    const existingProduct = productsList.find((product) => product.id === novoProduto.id);
+    if (existingProduct) {
+      const updatedProduct = {
+        ...existingProduct,
+        quantidade: isNaN(existingProduct.quantidade) ? 1 : existingProduct.quantidade + 1,
+      };
+      const updatedProductsList = productsList.map((product) =>
+        product.id === existingProduct.id ? updatedProduct : product
+      );
+      setProductsList(updatedProductsList);
+    } else {
+      setProductsList([...productsList, novoProduto]);
+    }
+  }
+
+  const removerProduto = (productToRemove: Product) => {
+    const updatedProductsList = productsList.map((product) => {
+      if (product.id === productToRemove.id) {
+        if (product.quantidade > 1) {
+          // Reduz a quantidade em 1 unidade
+          return { ...product, quantidade: product.quantidade - 1 };
+        } else {
+          // Remove o produto completamente
+          return null;
+        }
+      }
+      return product;
+    });
+
+    // Remove os elementos null (produtos removidos) do array
+    setProductsList(updatedProductsList.filter((product): product is Product => product !== null));
   };
 
-  const handleOptionChange = (event: React.ChangeEvent<{}>, newValue: Product) => {
-    setSelectedProduct(newValue);
-  };
-
-  const handlePageChange = (_: React.ChangeEvent<unknown>, newPage: number) => {
-    setPage(newPage);
-  };
-
-  const handleDeleteProduct = async (productId: number) => {
-    console.log(`handleDeleteProduct: ${productId}`);
+  const atualizarProduto = (updatedProduct: Product) => {
+    const updatedProductsList = productsList.map((product) =>
+      product.id === updatedProduct.id ? updatedProduct : product
+    );
+    setProductsList(updatedProductsList);
   };
 
   const handleSubmit = async () => {
@@ -257,51 +296,6 @@ Orçamento válido por 30 dias.
     }
   };
 
-  function adicionarProduto(novoProduto: Product) {
-    // Se o produto não tiver nenhuma quantidade, então atribua (ou atualize) quantidade para 1 ao adicioná-lo na lista
-    if (isNaN(novoProduto.quantidade) || novoProduto.quantidade === 0) {
-      novoProduto.quantidade = 1;
-    }
-    // Se já está na productsList, não adiciona novamente, soma 1 na sua quantidade.
-    const existingProduct = productsList.find((product) => product.id === novoProduto.id);
-    if (existingProduct) {
-      const updatedProduct = {
-        ...existingProduct,
-        quantidade: isNaN(existingProduct.quantidade) ? 1 : existingProduct.quantidade + 1,
-      };
-      const updatedProductsList = productsList.map((product) =>
-        product.id === existingProduct.id ? updatedProduct : product
-      );
-      setProductsList(updatedProductsList);
-    } else {
-      setProductsList([...productsList, novoProduto]);
-    }
-  }
-
-  const removerProduto = (productToRemove: Product) => {
-    const updatedProductsList = productsList.map((product) => {
-      if (product.id === productToRemove.id) {
-        if (product.quantidade > 1) {
-          // Reduz a quantidade em 1 unidade
-          return { ...product, quantidade: product.quantidade - 1 };
-        } else {
-          // Remove o produto completamente
-          return null;
-        }
-      }
-      return product;
-    });
-
-    // Remove os elementos null (produtos removidos) do array
-    setProductsList(updatedProductsList.filter((product): product is Product => product !== null));
-  };
-
-  const atualizarProduto = (updatedProduct: Product) => {
-    const updatedProductsList = productsList.map((product) =>
-      product.id === updatedProduct.id ? updatedProduct : product
-    );
-    setProductsList(updatedProductsList);
-  };
 
   return (
     <PageContainer title="Orçamento / Gerar" description="Gerar Orçamento da Arte Arena">
@@ -312,7 +306,7 @@ Orçamento válido por 30 dias.
             sx={{
               mt: 0,
             }}
-            htmlFor="titulo"
+            htmlFor="cliente"
           >
             Cliente
           </CustomFormLabel>
@@ -332,39 +326,38 @@ Orçamento válido por 30 dias.
           <div style={{ marginTop: '20px' }}>
             <div style={{ display: 'flex', justifyContent: 'flex-start', alignItems: 'center', marginBottom: '20px' }}>
               <div style={{ flex: 1 }}>
+
+                <CustomFormLabel
+                  sx={{
+                    mt: 0,
+                  }}
+                  htmlFor="produto"
+                >
+                  Produto
+                </CustomFormLabel>
+
                 {/* Campo de busca com botão */}
                 <Stack spacing={2} direction="row" alignItems="center" mb={2}>
-                  {/* <Autocomplete
+                  <Autocomplete
                     fullWidth
+                    id="produto"
+                    options={productNames}
+                    getOptionLabel={(option) => option}
+
                     // value={inputValue}
                     onChange={(event, selectedValue) => {
                       if (selectedValue) {
-                        setInputValue(selectedValue.nome); // Update input value with selected product name
-                        setSelectedProduct(selectedValue); // Set selected product for adding
+                        const selectedProduct = optionsProducts.find((product) => product.nome === selectedValue);
+                        setSelectedProduct(selectedProduct ? selectedProduct : null); // Set selected product for adding
                       } else {
-                        setInputValue(query); // Update input value with search query on clear
                         setSelectedProduct(null); // Reset selected product
                       }
                     }}
-                    onInputChange={(event, newInputValue) => {
-                      setInputValue(newInputValue); // Update input value on user input
-                    }}
-                    options={}
-                    getOptionLabel={(option) => option.nome}
                     renderInput={(params) => (
                       <CustomTextField {...params} placeholder="Buscar produto..." aria-label="Buscar produto" />
                     )}
-                  /> */}
-                  <div>
-                    <Button
-                      variant="contained"
-                      color="primary"
-                      onClick={handleSearch}
-                      startIcon={<IconSearch />}
-                    >
-                      Buscar
-                    </Button>
-                  </div>
+                  />
+
                   <div style={{ flex: 1 }}>
                     <div style={{ marginLeft: '20px' }}>
                       <Button
@@ -377,6 +370,7 @@ Orçamento válido por 30 dias.
                             alert('Por favor, selecione um produto');
                           }
                         }}
+                        disabled={!selectedProduct}
                       >
                         Adicionar
                       </Button>
@@ -401,7 +395,17 @@ Orçamento válido por 30 dias.
                   {productsList.map((product: Product, index) => (
                     <TableRow key={index}>
 
-                      <TableCell>{product.nome}</TableCell>
+                      <TableCell>
+                        <CustomTextField
+                          value={product.nome}
+                          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+                            const updatedProduct = { ...product, nome: event.target.value };
+                            atualizarProduto(updatedProduct);
+                          }}
+                          variant="outlined"
+                          size="small"
+                        />
+                      </TableCell>
 
                       <TableCell align="right">
                         <NumericFormat
@@ -478,8 +482,18 @@ Orçamento válido por 30 dias.
               </Table>
             </TableContainer>
 
+            <CustomFormLabel
+              sx={{
+                mt: 5,
+              }}
+              htmlFor="transportadora"
+            >
+              Transportadora
+            </CustomFormLabel>
+
             <Box sx={{ display: 'flex', justifyContent: 'left', mt: 2 }}>
               <CustomTextField
+                id="transportadora"
                 label="CEP do cliente"
                 value={cep}
                 onChange={(event: React.ChangeEvent<HTMLInputElement>) => setCEP(event.target.value)}
