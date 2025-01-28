6b7b566585ee1b61b2de371ff1eccd62f19d7766
Leandro Damasio <ldamasio@gmail.com>
2025-01-28 11:12:53 -0300

    feat: Add shortlink resolver (2)

--- modernize/src/app/s/[code]/page.tsx
+++ modernize/src/app/s/[code]/page.tsx
@@ -33,12 +33,12 @@ export default function S({ params }: PageProps) {
     const data = await response.json();
 
     if (response.ok) {
-      // router.push(data.url);
       console.log(data.caminho);
       if (typeof data.caminho === 'number' && data.caminho > 0) {
         console.log('O caminho ', data.caminho, ' existe');
         const fullShortUrl = `${window.location.origin}/apps/orcamento/backoffice/cliente-cadastro?id=${data.caminho}`;
         console.log('Full Short URL:', fullShortUrl);
+        router.push(fullShortUrl);
       }
     } else {
       console.error('Erro ao resolver URL:', data.error);
