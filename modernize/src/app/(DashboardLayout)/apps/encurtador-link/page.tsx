6ba7a537bd5154370f530f70beeb52342ab30ced
Leandro Damasio <ldamasio@gmail.com>
2025-01-28 16:28:23 -0300

    feat: Finalize Link

--- modernize/src/app/(DashboardLayout)/apps/encurtador-link/page.tsx
+++ modernize/src/app/(DashboardLayout)/apps/encurtador-link/page.tsx
@@ -6,6 +6,14 @@ import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import CustomTextField from "@/app/components/forms/theme-elements/CustomTextField";
 import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+import IconButton from '@mui/material/IconButton';
+import { IconCopy } from '@tabler/icons-react';
+
+
+interface ShortUrlResponse {
+  code: string;
+}
 
 const BCrumb = [
   {
@@ -22,9 +30,14 @@ const EncurtadorLink = () => {
   const [shortUrl, setShortUrl] = useState("");
   const [error, setError] = useState("");
 
-  const handleFetchShortUrl = async (url: string) => {
+  const accessToken = localStorage.getItem('accessToken');
+  if (!accessToken) {
+    throw new Error('Access token is missing');
+  }
+
+  const handleFetchShortUrl = async (url: string): Promise<ShortUrlResponse | null> => {
     try {
-      const response = await fetch("/api/encurtador-de-link", {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API}/api/encurtador-link`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
@@ -36,7 +49,10 @@ const EncurtadorLink = () => {
         throw new Error("Erro ao encurtar a URL");
       }
 
-      const data = await response.json();
+      const data: ShortUrlResponse = await response.json();
+      const fullUrl = `${window.location.origin}/p/${data.code}`;
+      console.log(fullUrl);
+      setShortUrl(fullUrl);
       return data;
     } catch (err) {
       console.error(err);
@@ -45,15 +61,10 @@ const EncurtadorLink = () => {
     }
   };
 
-  const handleOnSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+  const handleOnSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
-    setError(""); // Limpa erros anteriores
-
     if (url) {
       const data = await handleFetchShortUrl(url);
-      if (data?.shortUrl) {
-        setShortUrl(data.shortUrl);
-      }
     }
   };
 
@@ -63,35 +74,61 @@ const EncurtadorLink = () => {
       <Typography variant="h5" gutterBottom>
         Encurtador de Link
       </Typography>
-      <form onSubmit={handleOnSubmit}>
-        <Grid container spacing={2}>
-          <Grid item xs={12} sm={12}>
-            <CustomTextField
-              label="URL"
-              value={url}
-              onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUrl(e.target.value)}
-              variant="outlined"
-              fullWidth
-            />
-          </Grid>
-          {error && (
-            <Grid item xs={12}>
-              <Typography color="error">{error}</Typography>
-            </Grid>
-          )}
-          <Grid item xs={12}>
-            <Button
-              type="submit"
-              variant="contained"
-              color="primary"
-              fullWidth
-            >
-              Encurtar
-            </Button>
-          </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={12} sm={12}>
+          <CustomTextField
+            onKeyPress={(e: React.KeyboardEvent<HTMLInputElement>) => {
+              const handleOnSubmit = async (e: React.MouseEvent<HTMLButtonElement>) => {
+                e.preventDefault();
+                if (url) {
+                  const data = await handleFetchShortUrl(url);
+                }
+              };
+            }}
+            label="URL"
+            value={url}
+            onChange={(e: React.ChangeEvent<HTMLInputElement>) => setUrl(e.target.value)}
+            variant="outlined"
+            fullWidth
+          />
         </Grid>
-      </form>
+        {error && (
+          <Grid item xs={12}>
+            <Typography color="error">{error}</Typography>
+          </Grid>
+        )}
+        <Grid item xs={12}>
+          <Button
+            fullWidth
+            variant="contained"
+            color="primary"
+            disabled={!url}
+            onClick={handleOnSubmit}
+          >
+            Encurtador
+          </Button>
+        </Grid>
+      </Grid>
+      <div>
+
+        {shortUrl && (
+          <Box sx={{ mt: 2, mb: 2, display: 'flex', justifyContent: 'center', alignItems: 'center', border: '1px solid grey', borderRadius: 1 }}>
+            {shortUrl && (
+              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <Typography variant="h6" sx={{ fontWeight: 'bold', mr: 1 }}>
+                  {shortUrl}
+                </Typography>
+                <IconButton onClick={() => { navigator.clipboard.writeText(shortUrl); }}>
+                  <IconCopy />
+                  <Typography variant="body2">Copiar Orçamento</Typography>
+                </IconButton>
+              </Box>
+            )}
+          </Box>
+        )}
+      </div>
     </PageContainer>
+
   );
 };
 
