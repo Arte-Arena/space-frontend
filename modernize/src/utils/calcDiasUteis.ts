e268b1e4898d1240c9c2b185fb1184fdebf8b863
Leandro Damasio <ldamasio@gmail.com>
2025-02-07 10:10:43 -0300

    feat: Create new Orcamente Entrega screen

---
+++ modernize/src/utils/calcDiasUteis.ts
@@ -0,0 +1,54 @@
+import { DateTime } from 'luxon';
+import avancarDias from './avancarDias';
+
+interface Feriado {
+  start_at: string;
+}
+
+interface ApiResponseFeriados {
+  dias_feriados: Feriado[];
+}
+
+function isWeekend(date: DateTime): boolean {
+  // 6 = Sábado, 7 = Domingo
+  const dayOfWeek = date.weekday;
+  return dayOfWeek === 6 || dayOfWeek === 7;
+}
+
+function isHoliday(date: DateTime, holidays: ApiResponseFeriados): boolean {
+  return holidays.dias_feriados.some(holiday => {
+    const holidayDate = DateTime.fromSQL(holiday.start_at);
+    return holidayDate.hasSame(date, 'day') &&
+      holidayDate.hasSame(date, 'month') &&
+      holidayDate.hasSame(date, 'year');
+  });
+}
+
+function isDiaUtil(date: DateTime, holidays: ApiResponseFeriados): boolean {
+  return !isWeekend(date) && !isHoliday(date, holidays);
+}
+
+export default function calcularDiasUteis(
+  dataInicial: DateTime,
+  diasUteisDesejados: number,
+  feriados: ApiResponseFeriados
+): DateTime {
+  let dataAtual = dataInicial;
+  let diasUteisContados = 0;
+
+  console.log(dataAtual);
+
+  while (diasUteisContados < diasUteisDesejados) {
+    // Avança um dia
+    dataAtual = avancarDias(dataAtual, 1);
+
+    // Se for dia útil, incrementa o contador
+    if (isDiaUtil(dataAtual, feriados)) {
+      diasUteisContados++;
+      console.log(`Dia útil contado: ${diasUteisContados}, Data Atual: ${dataAtual}`);
+
+    }
+  }
+
+  return dataAtual;
+}
