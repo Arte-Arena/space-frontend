3aa2115e751987d0f521909841240d4814434bf6
Pablo Yuri <pabloyuridev@gmail.com>
2025-03-03 12:46:00 -0300

    Add custom scroll bar

--- modernize/src/app/components/custom-scroll/Scrollbar.tsx
+++ modernize/src/app/components/custom-scroll/Scrollbar.tsx
@@ -5,8 +5,26 @@ import { SxProps } from '@mui/system';
 import { styled } from '@mui/material/styles'
 import { useMediaQuery } from "@mui/material";
 
-const SimpleBarStyle = styled(SimpleBar)(() => ({
+const SimpleBarStyle = styled(SimpleBar)(({ theme }) => ({
   maxHeight: "100%",
+  ".simplebar-scrollbar": {
+    "&:before": {
+      backgroundColor: theme.palette.mode === "dark" 
+        ? "rgba(255, 255, 255, 0.3)" 
+        : "rgba(0, 0, 0, 0.2)",
+      borderRadius: 8,
+    },
+    "&.simplebar-visible:before": {
+      opacity: 0.7,
+    },
+  },
+  ".simplebar-track.simplebar-vertical": {
+    width: "10px",
+    marginRight: "2px",
+  },
+  ".simplebar-track.simplebar-horizontal .simplebar-scrollbar": {
+    height: "6px",
+  },
 }));
 
 interface PropsType {
