6caf47f1a1d78a57e2516770edfd164f7284b0ab
Leandro Damasio <ldamasio@gmail.com>
2025-03-04 16:50:08 -0300

    feat: Implement loading to all buttons on Pacotes Uniformes

--- modernize/src/app/(DashboardLayout)/apps/produtos/pacotes-uniformes/add/page.tsx
+++ modernize/src/app/(DashboardLayout)/apps/produtos/pacotes-uniformes/add/page.tsx
@@ -1,4 +1,7 @@
 'use client'
+import Breadcrumb from '@/app/(DashboardLayout)/layout/shared/breadcrumb/Breadcrumb';
+import PageContainer from '@/app/components/container/PageContainer';
+import ParentCard from '@/app/components/shared/ParentCard';
 import ProdutoPacoteUniformeForm from "@/app/(DashboardLayout)/apps/produtos/pacotes-uniformes/ProdutoPacoteUniformeForm";
 import ProdutoPacoteUniforme from '../types';
 
@@ -9,11 +12,31 @@ export default function ProdutosPacotesUniformesAddScreen() {
     // Aqui, você pode enviar a atualização para a API
   }
 
+  const BCrumb = [
+    {
+      to: "/",
+      title: "Home",
+    },
+    {
+      to: "/apps/produtos/",
+      title: "Produtos",
+    },
+    {
+      to: "/apps/produtos/pacotes-uniformes",
+      title: "Pacotes de Uniformes",
+    },
+  ];
+
   return (
-    <div>
-      <h2 className="text-xl font-bold">Adicionar Novo Pacote de Uniformes</h2>
-      <ProdutoPacoteUniformeForm onSubmit={handleAdd} />
-    </div>
+    <PageContainer title="Produtos / Pacotes de Uniformes" description="Pacotes de Uniformes da Arte Arena">
+      <Breadcrumb title="Produtos / Pacotes de Uniformes" items={BCrumb} />
+      <ParentCard title="Pacotes de Uniformes">
+        <>
+          <h2 className="text-xl font-bold">Adicionar Novo Pacote de Uniformes</h2>
+          <ProdutoPacoteUniformeForm onSubmit={handleAdd} />
+        </>
+      </ParentCard>
+    </PageContainer>
   );
 
 }
--- modernize/src/app/(DashboardLayout)/apps/produtos/pacotes-uniformes/edit/[id]/page.tsx
+++ modernize/src/app/(DashboardLayout)/apps/produtos/pacotes-uniformes/edit/[id]/page.tsx
@@ -1,5 +1,8 @@
 'use client'
 import { useEffect, useState } from "react";
+import Breadcrumb from '@/app/(DashboardLayout)/layout/shared/breadcrumb/Breadcrumb';
+import PageContainer from '@/app/components/container/PageContainer';
+import ParentCard from '@/app/components/shared/ParentCard';
 import ProdutoPacoteUniformeForm from "@/app/(DashboardLayout)/apps/produtos/pacotes-uniformes/ProdutoPacoteUniformeForm";
 import ProdutoPacoteUniforme from '../../types';
 import { useQuery } from '@tanstack/react-query';
@@ -49,10 +52,31 @@ export default function ProdutosPacotesUniformesDetailsScreen() {
     // Aqui, você pode enviar a atualização para a API
   }
 
+
+  const BCrumb = [
+    {
+      to: "/",
+      title: "Home",
+    },
+    {
+      to: "/apps/produtos/",
+      title: "Produtos",
+    },
+    {
+      to: "/apps/produtos/pacotes-uniformes",
+      title: "Pacotes de Uniformes",
+    },
+  ];
+
   return (
-    <div>
-      <h2 className="text-xl font-bold">Detalhes do Produto</h2>
-      <ProdutoPacoteUniformeForm initialData={pacoteUniforme} onSubmit={handleUpdate} />
-    </div>
+    <PageContainer title="Produtos / Pacotes de Uniformes" description="Pacotes de Uniformes da Arte Arena">
+      <Breadcrumb title="Produtos / Pacotes de Uniformes" items={BCrumb} />
+      <ParentCard title="Pacotes de Uniformes">
+        <>
+          <h2 className="text-xl font-bold">Detalhes do Produto</h2>
+          <ProdutoPacoteUniformeForm initialData={pacoteUniforme} onSubmit={handleUpdate} />
+        </>
+      </ParentCard>
+    </PageContainer>
   );
 }
