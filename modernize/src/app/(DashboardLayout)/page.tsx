616c6031d74ff80a63eb330e00e079e477044b83
Gutavo <gustavogarcia56336@gmail.com>
2025-03-05 14:44:39 -0300

    feat: add new dashboard charts in index

--- modernize/src/app/(DashboardLayout)/page.tsx
+++ modernize/src/app/(DashboardLayout)/page.tsx
@@ -4,6 +4,12 @@ import Box from '@mui/material/Box'
 import { useEffect, useState } from 'react';
 import PageContainer from '@/app/components/container/PageContainer';
 import { useAuth } from '@/utils/useAuth';
+import VendasRelatoriosOrcamentosPorStatusComponent from '../components/dashboard-index/ConversaoOrcamento';
+import VendasRelatoriosOrcamentosPorDataComponent from '../components/dashboard-index/OrcamentoPorDataMes';
+import { Grid } from '@mui/material';
+import VendasRelatoriosOrcamentosComponent from '../components/dashboard-index/orcamentoQuantidade';
+import VendasRelatoriosQuantidadeOrcamentosComponent from '../components/dashboard-index/orcamentoQuantidadeAprovados';
+import ParentCard from "@/app/components/shared/ParentCard";
 
 export default function Dashboard() {
 
@@ -21,11 +27,44 @@ export default function Dashboard() {
     return <div>Loading...</div>;
   }
 
+  // aqui deve conter alguns dos charts do space
   return (
     <PageContainer title="Dashboard" description="this is Dashboard">
-      <Box mt={3}>
-        Página principal do Space.
-      </Box>
+      <ParentCard title="Relatórios">
+        {/* Primeira linha: Um gráfico maior */}
+        <Grid container spacing={2}>
+
+          <Grid item xs={12} md={7}>
+            <Box sx={{ border: '1px solid #394457', padding: 2, height: '85%' }}>
+              <VendasRelatoriosOrcamentosPorDataComponent />
+            </Box>
+          </Grid>
+
+          <Grid item xs={12} md={5}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', height: '85%', border: '1px solid #394457', padding: 2 }}>
+              <VendasRelatoriosOrcamentosPorStatusComponent />
+              <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
+                <VendasRelatoriosQuantidadeOrcamentosComponent />
+                <VendasRelatoriosOrcamentosComponent />
+              </Box>
+            </Box>
+          </Grid>
+
+          {/* Segunda linha: Três gráficos menores */}
+          <Grid item xs={12} md={4}>
+            {/* <VendasRelatoriosOrcamentosPorStatusComponent /> */}
+          </Grid>
+
+          {/* <Grid item xs={12} md={4}>
+              <VendasRelatoriosOutroComponente />
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <VendasRelatoriosMaisUmComponente />
+            </Grid> */}
+
+
+        </Grid>
+      </ParentCard>
     </PageContainer>
   )
 }
---
+++ modernize/src/app/components/dashboard-index/ConversaoOrcamento.tsx
@@ -0,0 +1,70 @@
+'use client'
+import React, { useState, useEffect } from "react";
+import { useQuery } from '@tanstack/react-query';
+import ApexOrcamentoStatus from "@/app/components/charts/quantidadeOrcamentoStatus";
+
+interface OrcamentoStatus {
+  aprovados: number;
+  naoAprovados: number;
+}
+
+const VendasRelatoriosOrcamentosPorStatusComponent = () => {
+  const [dados, setDados] = useState<OrcamentoStatus | null>(null);
+  const [aprovados, setAprovados] = useState<number>(0);
+  const [naoAprovados, setNaoAprovados] = useState<number>(0);
+
+  // Pega token do localStorage
+  const accessToken = typeof window !== "undefined" ? localStorage.getItem('accessToken') : null;
+
+  if (!accessToken) {
+    console.error('Access token is missing');
+  }
+
+  // Fetch da API usando react-query
+  const { data, isFetching, error } = useQuery({
+    queryKey: ['ConversaoOrcamento'],
+    queryFn: async () => {
+      if (!accessToken) {
+        throw new Error('Token de acesso não disponível');
+      }
+
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API}/api/vendas/orcamentos-por-status`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      if (!res.ok) {
+        throw new Error("Erro ao buscar dados");
+      }
+
+      return res.json();
+    },
+    enabled: !!accessToken, // Só executa a query se houver token
+  });
+
+
+  useEffect(() => {
+    if (data) {
+      // Verificar se os dados são válidos
+      setDados(data);
+      setAprovados(data.aprovados || 0); // Garantir que aprovados seja um número
+      setNaoAprovados(data.naoAprovados || 0); // Garantir que naoAprovados seja um número
+    }
+  }, [data]);
+
+
+  return (
+    <div style={{ marginBottom: '50px' }}>
+      {dados ? (
+        <ApexOrcamentoStatus aprovados={aprovados} naoAprovados={naoAprovados} />
+      ) : (
+        <div>Sem dados disponíveis para exibir.</div>
+      )}
+    </div>
+  );
+};
+
+export default VendasRelatoriosOrcamentosPorStatusComponent;
