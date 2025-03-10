9e24a5cfee516efb398cf60bc8aa5cfe86bf97ee
Gutavo <gustavogarcia56336@gmail.com>
2025-03-10 17:11:48 -0300

    fix: add new type for the useQuery, fix the importion and exportion names, and erased axios from importion

---
+++ modernize/src/app/(DashboardLayout)/apps/producao/arte-final/components/types.tsx
@@ -0,0 +1,48 @@
+interface ApiResponsePedidosArteFinal {
+    data: ArteFinal[];
+    total: number;
+}
+
+
+interface ArteFinal {
+    id?: number;
+    user_id?: number | null;
+    numero_pedido: Number;
+    data_entrega: Date;
+    prazo_arte_final: Number;
+    prazo_confeccao: Number;
+    lista_produtos: Produto[];
+    observacao: string;
+    rolo: string;
+    estagio?: string | null;
+    designer?: number | null;
+    pedido_status_id?: number | null;
+    pedido_tipo_id?: number | null;
+    observacoes?: string | null;
+    status: string;
+    url_trello?: string | null;
+    prioridade?: string | null;
+    orcamento_id?: number | null;
+    tipo_de_pedido: string;
+    created_at: Date;
+    updated_at: Date;
+}
+
+interface Produto {
+    uid?: number;
+    id: number;
+    nome: string;
+    esboco: string;
+    quantidade: number;
+    materiais: Material[];
+    medida_linear: number;
+    preco: number;
+    prazo: number;
+}
+
+interface Material {
+    id: number;
+    descricao: string;
+}
+
+export type { ApiResponsePedidosArteFinal, ArteFinal, Produto, Material };
