5a699a0cc775600828e0f4c326ebd967dbb5c024
Gutavo <gustavogarcia56336@gmail.com>
2025-03-11 09:54:13 -0300

    feat: add names for the id fields

--- modernize/src/app/(DashboardLayout)/apps/producao/arte-final/components/drawer.tsx
+++ modernize/src/app/(DashboardLayout)/apps/producao/arte-final/components/drawer.tsx
@@ -1,13 +1,37 @@
 import React from "react";
 import { Drawer, Box, Typography, IconButton } from "@mui/material";
 import CloseIcon from "@mui/icons-material/Close";
-import { ArteFinal } from "../types";
+import { ArteFinal } from "./types";
 
 interface SidePanelProps {
   row: ArteFinal | null;
   openDrawer: boolean;
   onCloseDrawer: () => void;
 }
+
+const pedidoTipos = {
+  1: 'Prazo normal',
+  2: 'Antecipação',
+  3: 'Faturado',
+  4: 'Metade/Metade',
+  5: 'Amostra',
+} as const;
+
+const pedidoStatus = {
+  1: { nome: 'Pendente', fila: 'D' },
+  2: { nome: 'Em andamento', fila: 'D' },
+  3: { nome: 'Arte OK', fila: 'D' },
+  4: { nome: 'Em espera', fila: 'D' },
+  5: { nome: 'Cor teste', fila: 'D' },
+  8: { nome: 'Pendente', fila: 'I' },
+  9: { nome: 'Processando', fila: 'I' },
+  10: { nome: 'Renderizando', fila: 'I' },
+  11: { nome: 'Impresso', fila: 'I' },
+  12: { nome: 'Em Impressão', fila: 'I' },
+  13: { nome: 'Separação', fila: 'I' },
+  14: { nome: 'Em Transporte', fila: 'E' },
+  15: { nome: 'Entregue', fila: 'E' },
+} as const;
 
 
 const SidePanel: React.FC<SidePanelProps> = ({ row,  openDrawer, onCloseDrawer }) => {
