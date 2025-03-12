29c3c9fc5e8367053a29c76c0ee25d7b795cd74d
Gutavo <gustavogarcia56336@gmail.com>
2025-03-12 17:26:38 -0300

    feat: add observacao dialog,and styles for the fields

---
+++ modernize/src/app/(DashboardLayout)/apps/producao/arte-final/components/observacaoDialog.tsx
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import {
+    Dialog,
+    DialogTitle,
+    DialogContent,
+    DialogActions,
+    Button,
+    FormControl,
+    Typography,
+    TextField,
+} from "@mui/material";
+import { ArteFinal } from "./types";
+import { useRouter } from 'next/navigation';
+
+interface DialogObsProps {
+    openDialogObs: boolean;
+    onCloseDialogObs: () => void;
+    row: ArteFinal | null;
+    refetch: () => void;
+
+}
+
+const DialogObs: React.FC<DialogObsProps> = ({
+    openDialogObs,
+    onCloseDialogObs,
+    row,
+    refetch
+}) => {
+    const [observacoes, setObservacoes] = useState<string | null>(null);
+    const router = useRouter();
+
+    const handleObsChange = () => {
+        if (!row?.id) {
+            console.error("ID do pedido não encontrado");
+            return;
+        }
+
+        // Chamada para a API para atribuir o designer ao pedido
+        const accessToken = localStorage.getItem("accessToken");
+        if (!accessToken) {
+            console.error("Usuário não autenticado");
+            return;
+        }
+
+        fetch(`${process.env.NEXT_PUBLIC_API}/api/producao/pedido-obs-change/${row.id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                Authorization: `Bearer ${accessToken}`,
+            },
+            body: JSON.stringify({ observacoes: observacoes }),
+        })
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Erro ao atribuir designer');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Designer atribuído com sucesso:', data);
+                refetch();
+                router.refresh();  // Atualiza os dados da página
+            })
+            .catch(error => {
+                console.error('Erro:', error);
+            });
+
+        onCloseDialogObs();
+    };
+
+    return (
+        <Dialog open={openDialogObs} onClose={onCloseDialogObs} fullWidth >
+            <DialogTitle>Atribuir Observação</DialogTitle>
+            <DialogContent sx={{ minHeight: 'fitContent', paddingTop: 2 }}>
+                <FormControl fullWidth>
+                    <TextField
+                        sx={{ marginTop: 1, marginBottom: 2 }}
+                        value={observacoes}
+                        onChange={(e) => setObservacoes(e.target.value)}
+                        placeholder="Observações"
+                        fullWidth
+                    />
+                </FormControl>
+            </DialogContent>
+            <DialogActions>
+                <Button onClick={onCloseDialogObs} color="secondary">
+                    Cancelar
+                </Button>
+                <Button onClick={handleObsChange} color="primary" variant="contained">
+                    Confirmar
+                </Button>
+            </DialogActions>
+        </Dialog>
+    );
+};
+
+export default DialogObs;
