89fc09fcfce8ece8c929c5a72773e4fb1b3209ba
Gutavo <gustavogarcia56336@gmail.com>
2025-03-14 17:59:06 -0300

    fixed the confecction query

--- modernize/src/app/(DashboardLayout)/apps/producao/confeccao/page.tsx
+++ modernize/src/app/(DashboardLayout)/apps/producao/confeccao/page.tsx
@@ -67,7 +67,7 @@ const ConfeccaoScreen = () => {
   const { data: dataPedidos, isLoading: isLoadingPedidos, isError: isErrorPedidos, refetch } = useQuery<ApiResponsePedidosArteFinal>({
     queryKey: ['pedidos'],
     queryFn: () =>
-      fetch(`${process.env.NEXT_PUBLIC_API}/api/producao/get-pedidos-arte-final?fila=I`, {
+      fetch(`${process.env.NEXT_PUBLIC_API}/api/producao/get-pedidos-arte-final?fila=C`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
