173f386a11779e0557283a290d1884733b5077b3
Gutavo <gustavogarcia56336@gmail.com>
2025-03-17 14:51:21 -0300

    fix: fixed the refetch impotion

--- modernize/src/app/(DashboardLayout)/apps/producao/arte-final/page.tsx
+++ modernize/src/app/(DashboardLayout)/apps/producao/arte-final/page.tsx
@@ -563,7 +563,7 @@ const ArteFinalScreen = () => {
             <AssignDesignerDialog openDialogDesinger={openDialogDesinger} onCloseDialogDesinger={() => setOpenDialogDesinger(false)} row={selectedRowDesinger} refetch={refetch} />
           )}
           <DialogObs openDialogObs={openDialogObs} onCloseDialogObs={() => setOpenDialogObs(false)} row={selectedRowObs} refetch={refetch} />
-          <SidePanel openDrawer={openDrawer} onCloseDrawer={() => setOpenDrawer(false)} row={selectedRowSidePanel} />
+          <SidePanel openDrawer={openDrawer} onCloseDrawer={() => setOpenDrawer(false)} row={selectedRowSidePanel} refetch={refetch}/>
         </>
       </ParentCard>
     </PageContainer>
