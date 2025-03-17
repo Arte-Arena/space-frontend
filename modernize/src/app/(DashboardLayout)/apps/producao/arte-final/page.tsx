53509c68961d3835c1c2f376a68a5833aab5b8d4
Gutavo <gustavogarcia56336@gmail.com>
2025-03-17 15:07:47 -0300

    feat: add validation to methods enviarIpressao/Confeccao

--- modernize/src/app/(DashboardLayout)/apps/producao/arte-final/page.tsx
+++ modernize/src/app/(DashboardLayout)/apps/producao/arte-final/page.tsx
@@ -175,14 +175,23 @@ const ArteFinalScreen = () => {
   };
 
   const handleEnviarImpressora = async (row: ArteFinal) => {
-    const sucesso = await trocarStatusPedido(row?.id, 8, refetch);
-    if (sucesso) {
-      console.log("Pedido enviado com sucesso!");
-      alert('sucesso');
+    const confirmar = window.confirm('Deseja enviar o pedido N° ' + row.numero_pedido +' para Impressão?');
+
+    if (confirmar) {
+      const sucesso = await trocarStatusPedido(row?.id, 8, refetch);
+      if (sucesso) {
+        console.log("Pedido enviado com sucesso!");
+        alert('Sucesso');
+      } else {
+        console.log("Falha ao enviar pedido.");
+        alert('Falha ao enviar pedido.');
+      }
     } else {
-      console.log("Falha ao enviar pedido.");
+      console.log("Envio cancelado.");
+      alert('Envio cancelado.');
     }
   };
+  
 
   // handles dos selects
   const handleStatusChange = async (row: ArteFinal, status_id: number) => {
