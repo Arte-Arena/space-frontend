2de7ca594b1447430f921ed644593aa2f522b7a7
Gustavo <gustavogarcia56336@gmail.com>
2025-04-14 13:21:52 -0300

    fix: Add validation to the data before setting it

--- modernize/src/app/(DashboardLayout)/page.tsx
+++ modernize/src/app/(DashboardLayout)/page.tsx
@@ -7,8 +7,6 @@ import { useAuth } from '@/utils/useAuth';
 import VendasRelatoriosOrcamentosPorStatusComponent from '../components/dashboard-index/ConversaoOrcamento';
 import VendasRelatoriosOrcamentosPorDataComponent from '../components/dashboard-index/OrcamentoPorDataMes';
 import { Grid } from '@mui/material';
-import VendasRelatoriosOrcamentosComponent from '../components/dashboard-index/orcamentoQuantidade';
-import VendasRelatoriosQuantidadeOrcamentosComponent from '../components/dashboard-index/orcamentoQuantidadeAprovados';
 import ParentCard from "@/app/components/shared/ParentCard";
 import ValorTotalPedidosComponent from '../components/dashboard-index/ValorTotalPedidos';
 
@@ -44,12 +42,8 @@ export default function Dashboard() {
 
           {/* 2° em pizza */}
           <Grid item xs={12} md={5}>
-            <Box sx={{ display: 'flex', flexDirection: 'column', height: '85%', border: '1px solid #394457', padding: 2 }}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', height: '85%', border: '1px solid #394457', padding: 2, alignItems: 'center', justifyContent: 'center' }}>
               <VendasRelatoriosOrcamentosPorStatusComponent />
-              <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-around' }}>
-                <VendasRelatoriosQuantidadeOrcamentosComponent />
-                <VendasRelatoriosOrcamentosComponent />
-              </Box>
             </Box>
           </Grid>
 
