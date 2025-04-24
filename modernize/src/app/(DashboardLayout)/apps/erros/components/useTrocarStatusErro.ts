e035591d864d83b154264b6d58ecc56afbdbb7d2
Gustavo <gustavogarcia56336@gmail.com>
2025-04-24 10:25:49 -0300

    feat: add all the handles for detalhes, status, and solucao

--- modernize/src/app/(DashboardLayout)/apps/erros/components/useTrocarStatusErro.ts
+++ modernize/src/app/(DashboardLayout)/apps/erros/components/useTrocarStatusErro.ts
@@ -1,31 +1,29 @@
 const trocarStatusErro = async (id: number | undefined, status_nome: string | undefined, refetch: () => void): Promise<boolean> => {
-  return false;
-  // try {
-  //   const accessToken = localStorage.getItem("accessToken");
-  //   if (!accessToken) throw new Error("Usuário não autenticado.");
+  try {
+    const accessToken = localStorage.getItem("accessToken");
+    if (!accessToken) throw new Error("Usuário não autenticado.");
 
-  //   const res = await fetch(
-  //     `${process.env.NEXT_PUBLIC_API}/api/producao/confeccao/corte-conferencia/status-corte-change`,
-  //     {
-  //       method: "PATCH",
-  //       headers: {
-  //         "Content-Type": "application/json",
-  //         Authorization: `Bearer ${accessToken}`,
-  //       },
-  //       body: JSON.stringify({ 
-  //         status: status_nome,
-  //         pedido_arte_final_id: id,
-  //        }),
-  //     }
-  //   );
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_API}/api/erros/status/${id}`,
+      {
+        method: "PATCH",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify({ 
+          status: status_nome,
+         }),
+      }
+    );
 
-  //   if (!res.ok) throw new Error("Erro ao alterar o status do pedido.");
-  //   refetch();
-  //   return true;
-  // } catch (error) {
-  //   console.error("Erro ao trocar status do pedido:", error);
-  //   return false; 
-  // }
+    if (!res.ok) throw new Error("Erro ao alterar o status do erro.");
+    refetch();
+    return true;
+  } catch (error) {
+    console.error("Erro ao trocar status do erro:", error);
+    return false; 
+  }
 };
 
 export default trocarStatusErro;
