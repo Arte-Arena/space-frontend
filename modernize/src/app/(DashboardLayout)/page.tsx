50ea7e7ffba133533187a0bf4fd826b48d32e10a
Gustavo <gustavogarcia56336@gmail.com>
2025-04-25 16:00:00 -0300

    add all fields in proportion with the width of the page

--- modernize/src/app/(DashboardLayout)/page.tsx
+++ modernize/src/app/(DashboardLayout)/page.tsx
@@ -1,6 +1,4 @@
 'use client';
-
-import Box from '@mui/material/Box';
 import { useEffect, useState } from 'react';
 import PageContainer from '@/app/components/container/PageContainer';
 import { useAuth } from '@/utils/useAuth';
@@ -8,25 +6,61 @@ import VendasRelatoriosOrcamentosPorStatusComponent from '../components/dashboar
 import VendasRelatoriosOrcamentosPorDataComponent from '../components/dashboard-index/OrcamentoPorDataMes';
 import ValorTotalPedidosComponent from '../components/dashboard-index/ValorTotalPedidos';
 import ResumoCard from '../components/dashboard-index/ResumeCards';
-
-import { Grid } from '@mui/material';
+import { Grid, Box } from '@mui/material';
 import {
   IconUsers,
   IconBuildingStore,
   IconShoppingCart,
-  IconBox
+  IconBox,
+  IconCurrencyBitcoin,
+  IconCurrencyDollar
 } from '@tabler/icons-react';
 
 export default function Dashboard() {
-  const isLoggedIn = useAuth();
-  const [isLoading, setLoading] = useState(true);
+  const [isLoading, setLoading] = useState(false);
+  const [quantidades, setQuantidades] = useState({
+    quantidade_pedidos: 0,
+    quantidade_clientes: 0,
+    quantidade_produtos: 0,
+    quantidade_orcamentos: 0,
+    valor_total_orcamentos: 0,
+    quantidade_funcionarios: 0,
+  });
 
+  const accessToken = typeof window !== "undefined" ? localStorage.getItem('accessToken') : null;
+  const isLoggedIn = useAuth();
   useEffect(() => {
     if (!isLoggedIn) {
       return;
     }
     setLoading(false);
   }, [isLoggedIn]);
+
+  useEffect(() => {
+    if (isLoggedIn) {
+      const fetchData = async () => {
+        try {
+          const response = await fetch(`${process.env.NEXT_PUBLIC_API}/api/vendas/quantidades`, {
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${accessToken}`,
+            },
+          });
+          if (!response.ok) {
+            throw new Error('Network response was not ok');
+          }
+          const data = await response.json();
+          console.log(data); // Substitua com a lógica apropriada para tratar os dados
+          setQuantidades(data);
+        } catch (error) {
+          console.error('Fetch error:', error);
+        }
+      };
+      fetchData();
+    }
+  }, [isLoggedIn]);
+
 
   if (isLoading) {
     return <div>Loading...</div>;
@@ -39,17 +73,26 @@ export default function Dashboard() {
         {/* Linha 1: Cards resumo */}
         <Grid item xs={12}>
           <Grid container spacing={2}>
-            <Grid item xs={12} sm={6} md={3}>
-              <ResumoCard titulo="Funcionários" valor={12} icone={<IconUsers size={24} />} cor="#1976d2" />
+            <Grid item xs={12} sm={6} md={2.4}>
+              <ResumoCard titulo="Funcionários" valor={quantidades.quantidade_funcionarios.toLocaleString('pt-BR')} icone={<IconUsers size={24} />} cor="#880e4f" />
             </Grid>
-            <Grid item xs={12} sm={6} md={3}>
-              <ResumoCard titulo="Clientes" valor={58} icone={<IconBuildingStore size={24} />} cor="#2e7d32" />
+            <Grid item xs={12} sm={6} md={2.4}>
+              <ResumoCard titulo="Clientes" valor={quantidades.quantidade_clientes.toLocaleString('pt-BR')} icone={<IconBuildingStore size={24} />} cor="#1976d2" />
             </Grid>
-            <Grid item xs={12} sm={6} md={3}>
-              <ResumoCard titulo="Pedidos" valor={102} icone={<IconShoppingCart size={24} />} cor="#ed6c02" />
+            <Grid item xs={12} sm={6} md={2.4}>
+              <ResumoCard titulo="Produtos" valor={quantidades.quantidade_produtos.toLocaleString('pt-BR')} icone={<IconBox size={24} />} cor="#4a148c" />
             </Grid>
-            <Grid item xs={12} sm={6} md={3}>
-              <ResumoCard titulo="Produtos" valor={37} icone={<IconBox size={24} />} cor="#6a1b9a" />
+            <Grid item xs={12} sm={6} md={2.4}>
+              <ResumoCard titulo="Pedidos" valor={quantidades.quantidade_pedidos.toLocaleString('pt-BR')} icone={<IconShoppingCart size={24} />} cor="#ed6c02" />
+            </Grid>
+            <Grid item xs={12} sm={6} md={2.4}>
+              <ResumoCard
+                titulo="Total De Pedidos"
+                valor={quantidades.valor_total_orcamentos.toLocaleString('pt-BR',
+                  { style: 'currency', currency: 'BRL' })}
+                icone={<IconCurrencyDollar size={24} />}
+                cor="#00695c"
+              />
             </Grid>
           </Grid>
         </Grid>
