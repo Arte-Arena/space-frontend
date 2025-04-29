f41fe80687b6ff2b07fea6944ce8f4108ac8f61f
Gustavo <gustavogarcia56336@gmail.com>
2025-04-29 13:26:46 -0300

    feat: add category, value, and weight filters

--- modernize/src/app/(DashboardLayout)/apps/produtos/buscar/page.tsx
+++ modernize/src/app/(DashboardLayout)/apps/produtos/buscar/page.tsx
@@ -12,12 +12,13 @@ import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
-import { Pagination, Stack, Button, Link, Typography } from '@mui/material';
+import { Pagination, Stack, Button, Link, Typography, Grid } from '@mui/material';
 import CustomTextField from '@/app/components/forms/theme-elements/CustomTextField';
 import InputAdornment from '@mui/material/InputAdornment';
-import { IconPencil, IconPencilCheck, IconSearch } from '@tabler/icons-react';
+import { IconCategory, IconEyeDollar, IconPencil, IconPencilCheck, IconSearch, IconWeight } from '@tabler/icons-react';
 import { useQuery } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
+import { LoadingButton } from '@mui/lab';
 
 interface Product {
   id: number;
@@ -33,6 +34,9 @@ interface Product {
 
 const ProdutosBuscarScreen = () => {
   const [query, setQuery] = useState<string>('');
+  const [cat, setCat] = useState<string>('');
+  const [preco, setPreco] = useState<string>('');
+  const [peso, setPeso] = useState<string>('');
   const [searchQuery, setSearchQuery] = useState<string>(''); // Mantém a query atual usada na API
   const [page, setPage] = useState<number>(1);
   const router = useRouter();
@@ -44,10 +48,10 @@ const ProdutosBuscarScreen = () => {
     router.push('/auth/login');
   }
 
-  const { isFetching, error, data } = useQuery({
+  const { isFetching, error, data, refetch } = useQuery({
     queryKey: ['repoData', searchQuery, page],
     queryFn: () =>
-      fetch(`${process.env.NEXT_PUBLIC_API}/api/produto?q=${encodeURIComponent(searchQuery)}&page=${page}`, {
+      fetch(`${process.env.NEXT_PUBLIC_API}/api/produto?q=${encodeURIComponent(searchQuery)}&page=${page}&cat=${cat}&preco=${preco}&peso=${peso}`, {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
@@ -56,9 +60,19 @@ const ProdutosBuscarScreen = () => {
       }).then((res) => res.json()),
   });
 
+  const handleCleanSearch = () => {
+    setQuery('');
+    setCat('');
+    setPeso('');
+    setPreco('');
+    setPage(1); 
+    setSearchQuery('');
+    // refetch();
+  };
   const handleSearch = () => {
-    setSearchQuery(query); // Atualiza a busca
+    setSearchQuery(query);
     setPage(1); // Reseta para a primeira página ao realizar uma nova busca
+    refetch();
   };
 
   const handleSearchKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
@@ -86,34 +100,103 @@ const ProdutosBuscarScreen = () => {
     <PageContainer title="Produtos / Buscar" description="Produtos da Arte Arena">
       <Breadcrumb title="Produtos / Buscar" subtitle="Gerencie Produtos da Arte Arena / Buscar" />
       <ParentCard title="Buscar Produto">
-
         <>
-
           {/* Campo de busca com botão */}
-          <Stack spacing={2} direction="row" alignItems="center" mb={2}>
-            <CustomTextField
-              fullWidth
-              value={query}
-              onChange={(event: React.ChangeEvent<HTMLInputElement>) => setQuery(event.target.value)}
-              onKeyPress={handleSearchKeyPress}
-              placeholder="Buscar produto..."
-              InputProps={{
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <IconSearch />
-                  </InputAdornment>
-                ),
-              }}
-            />
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={handleSearch}
-              startIcon={<IconSearch />}
-            >
-              Buscar
-            </Button>
-          </Stack>
+          <Grid container spacing={2} alignItems="center" mb={2}>
+            {/* 1️⃣ Primeiro campo ocupa 4/12 colunas */}
+            <Grid item xs={12} sm={4}>
+              <CustomTextField
+                fullWidth
+                value={query}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setQuery(e.target.value)}
+                onKeyPress={handleSearchKeyPress}
+                placeholder="Buscar por nome ou código..."
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <IconSearch />
+                    </InputAdornment>
+                  )
+                }}
+              />
+            </Grid>
+
+            {/* 2️⃣,3️⃣,4️⃣ Próximos campos ocupam 2/12 colunas cada */}
+            <Grid item xs={12} sm={2}>
+              <CustomTextField
+                fullWidth
+                onKeyPress={handleSearchKeyPress}
+                value={cat}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setCat(e.target.value)}
+                placeholder="Categoria..."
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <IconCategory />
+                    </InputAdornment>
+                  )
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={2}>
+              <CustomTextField
+                fullWidth
+                onKeyPress={handleSearchKeyPress}
+                value={preco}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPreco(e.target.value)}
+                placeholder="Preço..."
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <IconEyeDollar />
+                    </InputAdornment>
+                  )
+                }}
+              />
+            </Grid>
+            <Grid item xs={12} sm={2}>
+              <CustomTextField
+                fullWidth
+                onKeyPress={handleSearchKeyPress}
+                value={peso}
+                onChange={(e: React.ChangeEvent<HTMLInputElement>) => setPeso(e.target.value)}
+                placeholder="Peso..."
+                InputProps={{
+                  startAdornment: (
+                    <InputAdornment position="start">
+                      <IconWeight />
+                    </InputAdornment>
+                  )
+                }}
+              />
+            </Grid>
+
+            {/* 5️⃣ Botão ocupa 1/12 colunas */}
+            <Grid item xs={12} sm={1}>
+              <LoadingButton
+                fullWidth
+                variant="contained"
+                color="primary"
+                onClick={handleSearch}
+                loading={isFetching}
+                startIcon={<IconSearch />}
+              >
+                Buscar
+              </LoadingButton>
+            </Grid>
+
+            <Grid item xs={12} sm={1}>
+              <Button
+                fullWidth
+                variant="outlined"
+                color="primary"
+                onClick={handleCleanSearch}
+                startIcon={<IconSearch />}
+              >
+                Limpar
+              </Button>
+            </Grid>
+          </Grid>
 
           {/* Tabela */}
           <TableContainer>
